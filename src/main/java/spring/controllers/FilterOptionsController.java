8f7135625ad37acdd9606a0dbc41ebfce8154b36
mmandy <mmandy@usf.edu>
2023-10-15 19:29:46 -0400

    Did some Rest Stuff

--- src/main/java/spring/controllers/FilterOptionsController.java
+++ src/main/java/spring/controllers/FilterOptionsController.java
@@ -1,20 +1,25 @@
 package spring.controllers;
 
 import connections.databaseOperations.SQLWriter;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController("/filter")
+@RestController()
+@RequestMapping("/filter")
 public class FilterOptionsController {
 
+    @RequestMapping(value = "/foodType", method = RequestMethod.GET)
+    public List<String> getFoodTypeOptions()
+    {
+        List<String> results = new ArrayList<>();
+        results.add("Italian");
+        results.add("Southern");
+        results.add("Cajun");
+        results.add("Chinese");
+        return results;
+    }
 
-//    @RequestMapping(value = "/foodType", method = RequestMethod.GET)
-//    public List<String> getFoodTypeOptions()
-//    {
-//
-//    }
+
 }

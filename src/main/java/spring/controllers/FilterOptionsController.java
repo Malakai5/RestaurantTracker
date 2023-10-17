c3d9464e2ebb81d33f650fcf9440380d7844ee13
mmandy <mmandy@usf.edu>
2023-10-17 16:35:28 -0400

    Some updates to SearchPage and Rest controllers

--- src/main/java/spring/controllers/FilterOptionsController.java
+++ src/main/java/spring/controllers/FilterOptionsController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:63342")
 @RestController()
 @RequestMapping("/filter")
 public class FilterOptionsController {
